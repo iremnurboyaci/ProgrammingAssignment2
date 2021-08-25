6fec234caad040901c6220526022543b1250de60
iremnurboyaci <89485372+iremnurboyaci@users.noreply.github.com>
2021-08-25 14:31:50 +0300

    Add files via upload

---
+++ Lexical Scoping - İremnur Boyacı.R
@@ -0,0 +1,39 @@
+# makeCacheMatrix
+##makeCacheMatrix do
+
+##first step
+
+makeCacheMatrix <- function(x = matrix()) {
+  z <- NULL
+  set <- function(y){
+    x <<- y
+    z <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) z <<- inverse
+  getInverse <- function() z 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+}
+
+
+
+
+## second step
+
+cacheSolve <- function(x, ...) {
+  ## Returning matrix
+  z <- x$getInverse()
+  if(!is.null(z)){
+    message(" cached data was got")
+    return(z)
+  }
+  mat <- x$get()
+  j <- solve(mat,...)
+  x$setInverse(z)
+  j
+}
+
+
+

c713d9d678ddf97cccd353ad5d12d71b3e031008
iremnurboyaci <89485372+iremnurboyaci@users.noreply.github.com>
2021-08-29 21:49:06 +0300

    Update Lexical Scoping - İremnur Boyacı.R

--- Lexical Scoping - İremnur Boyacı.R
+++ Lexical Scoping - İremnur Boyacı.R
@@ -1,32 +1,28 @@
 ## simply set the input x as a matrix
-## and then set the solved value "s" as a null
-## then  changed every reference to "mean" to "solve"
-makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
-  s <- NULL
-  set <- function(y) {
-    x <<- y
-    s <<- NULL
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+  x <<- y
+  inv <<- NULL
   }
-  get <- function() x
-  setsolve <- function(solve) s <<- solve
-  getsolve <- function() s
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+  get <- function()x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get, 
+  setInverse = setInverse, 
+  getInverse = getInverse)
 }
-##
-##  changed "mean" to "solve" and "m" to "s"
+
 cacheSolve <- function(x, ...) {
-  s <- x$getsolve()
-  if(!is.null(s)) {
-    message("getting inversed matrix")
-    return(s)
+ ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+  message("getting cached data")
+  return(inv)
   }
-  data <- x$get()
-  s <- solve(data, ...)
-  x$setsolve(s)
-  s
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
-
-
 
